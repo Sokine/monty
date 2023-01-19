9c4063a6403ad8aec280782bf2add95456e65e8d
Sokine <sokine_25@outlook.com>
2023-01-19 06:26:50 -0800

    Task 1

--- funct_1.c
+++ funct_1.c
@@ -9,28 +9,28 @@
  */
 void push(stack_t **stack, unsigned int nline)
 {
-	stack_t *new;
+        stack_t *new;
 
-	if (stack == NULL)
-	{
-		fprintf(stderr, "L%d: stack not found\n", nline);
-		exit(EXIT_FAILURE);
-	}
+        if (stack == NULL)
+        {
+                fprintf(stderr, "L%d: stack not found\n", nline);
+                exit(EXIT_FAILURE);
+        }
 
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		free_stack(stack);
-		exit(EXIT_FAILURE);
-	}
+        new = malloc(sizeof(stack_t));
+        if (new == NULL)
+        {
+                fprintf(stderr, "Error: malloc failed\n");
+                free_stack(stack);
+                exit(EXIT_FAILURE);
+        }
 
-	new->next = *stack;
-	new->prev = NULL;
-	new->n = arg.arg;
-	if (*stack)
-		(*stack)->prev = new;
-	*stack = new;
+        new->next = *stack;
+        new->prev = NULL;
+        new->n = arg.arg;
+        if (*stack)
+                (*stack)->prev = new;
+        *stack = new;
 }
 
 /**
@@ -42,15 +42,15 @@ void push(stack_t **stack, unsigned int nline)
  */
 void pall(stack_t **stack, unsigned int nline)
 {
-	stack_t *temp;
-	(void)nline;
+        stack_t *temp;
+        (void)nline;
 
-	temp = *stack;
-	while (temp)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
-	}
+        temp = *stack;
+        while (temp)
+        {
+                printf("%d\n", temp->n);
+                temp = temp->next;
+        }
 }
 
 /**
@@ -61,17 +61,17 @@ void pall(stack_t **stack, unsigned int nline)
  */
 void free_stack(stack_t **stack)
 {
-	stack_t *temp = NULL;
+        stack_t *temp = NULL;
 
-	if (stack == NULL || *stack == NULL)
-		return;
+        if (stack == NULL || *stack == NULL)
+                return;
 
-	while (*stack != NULL)
-	{
-		temp = (*stack)->next;
-		free(*stack);
-		*stack = temp;
-	}
+        while (*stack != NULL)
+        {
+                temp = (*stack)->next;
+                free(*stack);
+                *stack = temp;
+        }
 }
 
 /**
@@ -82,8 +82,8 @@ void free_stack(stack_t **stack)
  */
 void nop(stack_t **stack, unsigned int nline)
 {
-	(void)stack;
-	(void)nline;
+        (void)stack;
+        (void)nline;
 }
 
 /**
@@ -93,8 +93,9 @@ void nop(stack_t **stack, unsigned int nline)
  */
 int _isalpha(int c)
 {
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-		return (1);
-	else
-		return (0);
+        if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+                return (1);
+        else
+                return (0);
 }
+
